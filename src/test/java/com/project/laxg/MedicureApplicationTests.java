d907b0beef95860d59232e1a4c6fae14da8f63a9
loks66 <laxmikantag903@gmail.com>
2025-10-10 11:24:43 +0000

    added all files

---
+++ src/test/java/com/project/laxg/MedicureApplicationTests.java
@@ -0,0 +1,25 @@
+package com.project.laxg;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.project.laxg.MedicureService;
+
+@SpringBootTest
+class MedicureApplicationTests {
+
+	@Autowired
+	MedicureService doctorService;
+	
+	@Test
+	void contextLoads() {
+	}
+	
+	@Test
+	void testService() {
+	  assertEquals(doctorService.sayHello(), "Hello from Doctor Shubham");
+	}
+}
